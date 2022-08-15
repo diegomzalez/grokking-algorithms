56b8f32a616d775831ee39a9651f3bf4cfc64778
Diego González <diegom.develops@hotmail.com>
2022-08-15 11:41:24 -0400

    fix: selection sort
    Removing an unused variable

--- algorithms/sort-algorithms/selection-sort/src/selectionSort.js
+++ algorithms/sort-algorithms/selection-sort/src/selectionSort.js
@@ -3,7 +3,6 @@ import findSmallest from "./findSmallest.js";
 export default function (arr) {
   let newArr = [];
   for (let index = arr.length; index > 0; index--) {
-    const element = arr[index];
     const smallest = findSmallest(arr);
     newArr.push(arr[smallest]);
     arr.splice(smallest, smallest + 1);
