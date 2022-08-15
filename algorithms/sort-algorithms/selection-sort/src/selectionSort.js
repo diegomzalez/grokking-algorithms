571bd66983366b85f64c72dbf23b4e9402b818da
Diego González <diegom.develops@hotmail.com>
2022-08-15 11:34:31 -0400

    fix: selection sort
    Fixing for login in selection sort

--- algorithms/sort-algorithms/selection-sort/src/selectionSort.js
+++ algorithms/sort-algorithms/selection-sort/src/selectionSort.js
@@ -2,12 +2,11 @@ import findSmallest from "./findSmallest.js";
 
 export default function (arr) {
   let newArr = [];
-  for (let index = 0; index < arr.length; index++) {
+  for (let index = arr.length; index > 0; index--) {
     const element = arr[index];
     const smallest = findSmallest(arr);
     newArr.push(arr[smallest]);
     arr.splice(smallest, smallest + 1);
-    index--;
   }
   return newArr;
 }
